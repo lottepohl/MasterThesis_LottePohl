2e28760bfcebdd9c2e0a596d42dc4518ad3cd343
lottepohl <lotte.pohl@imbrsea.eu>
2023-04-04 11:21:33 +0200

    start with custom figure theme

---
+++ 01_code/05_plots_maps/thesis_manuscript_figures.R
@@ -0,0 +1,39 @@
+# Script to generate figures for the thesis manuscript
+
+# Workspace ####
+
+## libraries ####
+
+library(ggplot2)
+
+## load data ####
+
+paste0(getwd(), "/01_code/06_functions/functions.R")
+paste0(getwd(), "/01_code/02_load_data/load_dst_summarystatistics.R") %>% base::source()
+
+# set plot theme ####
+
+thesis_theme <- theme(
+  plot.title = element_text(family = "serif", size = 12, face = "bold"),
+  plot.subtitle = element_text(family = "serif", size = 12),
+  axis.title = element_text(family = "serif", size = 12),
+  axis.text = element_text(family = "serif", size = 10),
+  legend.title = element_text(family = "serif", size = 12),
+  legend.text = element_text(family = "serif", size = 10),
+  # plot.background = element_blank()#,
+  panel.background = element_blank(),
+  # panel.background = element_rect(fill = "transparent"),
+  panel.grid.major = element_line(color = "gray70", linetype = "solid"),
+  panel.grid.minor = element_line(color = "gray90", linetype = "dashed"),
+)
+
+# Set the theme as the default for all plots
+theme_set(thesis_theme)
+
+
+# first plot attempt ####
+ggplot(data = long_dst_date,
+       mapping = aes(x = date, y = depth_median)) +
+  geom_line() +
+  labs(x = "Date", y = "Depth in m")
+
