7247692464a673fc8595179210737ec6a8b2a180
lottepohl <lotte.pohl@imbrsea.eu>
2023-04-07 20:56:27 +0200

    finish wavelet analysis

--- 01_code/x_experimental/wavelet_plot_test3.R
+++ 01_code/x_experimental/wavelet_plot_test3.R
@@ -16,14 +16,30 @@ paste0(dir_path_new, "/01_code/02_load_data/load_autocorrelation_results.R") %>%
 dates_308 <- long_dst_date %>% filter(tag_serial_number == "1293308") %>% dplyr::select(date)
 
 wt_df_308_depthrange <- wavelet_output_compare(dates = dates_308, wt_output = wt_308_depthrange) #wt_308_depthrange$xaxis %>% as.data.frame()
+wt_df_308_mediandepth <- wavelet_output_compare(dates = dates_308, wt_output = wt_308_mediandepth) #wt_308_depthrange$xaxis %>% as.data.frame()
 
-p_wave_308_depthchange <- plot_wavelet_gg2(wt_df = wt_df_308_depthrange, type = "power_log", date = T)
+p_wave_308_depthchange <- plot_wavelet_gg2(wt_df = wt_df_308_depthrange, type = "power_log", date = F)
 p_wave_308_depthchange #%>% ggplotly()
 
-# my_function <- function(dataframe, x_value) {
-#   ggplot(data = dataframe, aes_string(x = x_value, y = "y")) +
-#     geom_point()
-# }
+p_wave_308_mediandepth <- plot_wavelet_gg2(wt_df = wt_df_308_mediandepth, type = "power_log", date = F)
+p_wave_308_mediandepth #%>% ggplotly()
+
+
+ggplot(data = wt_df) +
+  geom_tile(aes(x = date, y = period, fill = power_log),
+            position = "identity",
+            alpha = 0.65) +
+  geom_tile(data = wt_df %>% filter(sig == 1), aes(x = date, y = period, fill = power_log),
+            position = "identity") +
+  scale_y_continuous(trans = my_trans,
+                     breaks = y_breaks, 
+                     expand = c(0,0)) +
+  scale_x_discrete(breaks = x_breaks) +
+  scale_fill_viridis_c(direction = 1, option = "turbo") +
+  # scale_fill_distiller(palette ="RdYlGn") +
+  labs(x = "dates", y = "period in hours", fill = "log2(power)") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 0.5))
+
 
 my_function <- function(dataframe, x_value) {
   ggplot(data = dataframe, aes(x = {{x_value}}, y = y)) +
